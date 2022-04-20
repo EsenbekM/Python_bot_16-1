1b139978d01d37ae61deb2c29635f13f659369d6
Esen <esenbekm03@gmail.com>
2022-04-20 19:43:32 +0600

    lesson 5

--- handlers/notification.py
+++ handlers/notification.py
@@ -1,12 +1,27 @@
 from aiogram import types, Dispatcher
 from config import bot, dp
 
+from lists import bad_words as bw
+import asyncio
+import aioschedule
+
+async def wake_up():
+    video = open("media/erjan.mp4", "rb")
+    await bot.send_video(chat_id=chat_id, video=video, caption="Эржан вставай!!!")
+
+async def scheduler(time):
+    aioschedule.every().day.at(time).do(wake_up)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
 
 # @dp.message_handler()
 async def echo_message(message: types.Message):
+    global chat_id
+    chat_id = message.chat.id
 
     # Check bad words
-    bad_words = "java bitch дурак балбес эшек".split()
+    bad_words = bw
 
     for i in bad_words:
         if i in message.text.lower():
@@ -19,9 +34,11 @@ async def echo_message(message: types.Message):
     if message.text.lower() == 'dice':
         await bot.send_dice(message.chat.id, emoji="🎯")
 
-    # pin message
-    # if message.text.startswith('pin'):
-    #     await bot.send_sticker(message.chat.id, message.message_id)
+    # notification
+    if message.text.startswith("разбуди меня в "):
+        await message.reply("ok")
+        await scheduler(message.text.replace("разбуди меня в ", ''))
+
 
 def register_hendlers_notification(dp: Dispatcher):
     dp.register_message_handler(echo_message)
---
+++ task.py
@@ -0,0 +1,37 @@
+"""
+
+Сравнить средний балл всех учеников со своим баллом,
+если ваш балл ниже среднего, вывести False,
+а если больше, вывести True.
+
+"""
+
+
+# def check_student_points(class_points, student_point):
+#     return True if (sum(class_points) // len(class_points)) < student_point else False
+#
+#     # sr = sum(class_points) // len(class_points)
+#     # if sr > student_point:
+#     #     return False
+#     # else:
+#     #     return True
+#
+# check_student_points(class_points=[55, 90, 23, 54, 76, 12], student_point=60)
+
+
+"""
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321
+
+"""
+
+# def Descending_Order(num):
+#     return int(''.join(sorted(list(str(num)), reverse=True)))
+#
+# print(Descending_Order(10789164535638170814037658376))
+
