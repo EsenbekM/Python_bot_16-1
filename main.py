942e2073dcedb3c63ef9b4130ddfe9f808b6fffa
Esen <esenbekm03@gmail.com>
2022-04-27 19:35:37 +0600

    added youtube inline

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from aiogram import executor
 from config import dp
 import logging
-from handlers import callback, client, notification, fsmAdminGetUser
+from handlers import callback, client, notification, fsmAdminGetUser, inline
 from database import bot_db
+
 
 async def on_start_up(_):
     bot_db.sql_create()
 
+
 client.register_hendlers_client(dp)
 callback.register_hendlers_callback(dp)
+inline.register_handler_inline(dp)
 fsmAdminGetUser.register_hendler_fsmAdminGetUser(dp)
 
 notification.register_hendlers_notification(dp)
